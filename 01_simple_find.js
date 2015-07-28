654c5ca2699114b126cb668d81a481ded5b7285c
Dylan Zincone <dylan.zincone@gmail.com>
2015-07-28 11:08:38 -0600

    Updated second story

--- 01_simple_find.js
+++ 01_simple_find.js
@@ -2,3 +2,11 @@ var db = require('monk')('localhost/association-objects')
 var people = db.get('people')
 var employers = db.get('employers')
 var addresses = db.get('addresses')
+
+
+people.findOne({_id: "55b7a0ac55aaf02320743457"}).then(function(person){
+  addresses.findOne({_id: person.addressId}).then(function(address){
+    person.address = address
+    console.log(person)
+  })  
+})
--- 02_people_and_addresses.js
+++ 02_people_and_addresses.js
@@ -2,3 +2,24 @@ var db = require('monk')('localhost/association-objects')
 var people = db.get('people')
 var employers = db.get('employers')
 var addresses = db.get('addresses')
+
+var addressIds = [];
+people.find({}).then(function(people){
+
+  for(var i = 0; i<people.length; i++){
+    addressIds.push(people[i].addressId)
+  }
+  addressIds = addressIds
+  return addresses.find({_id: {$in: addressIds}})
+}).then(function(ids){
+
+    people.find({}).then(function(people){
+      for(var i = 0; i < people.length; i++){
+        for(var j = 0; j < ids.length; j++){
+          if(people[i].addressId.toString() === ids[j]._id.toString()){
+            people[i].address = ids[j]
+          }
+        }
+      }console.log(people)
+    })
+  })
