aea6f10999a22bf63a99c69f1c7f4aa02c656bfd
Dylan Zincone <dylan.zincone@gmail.com>
2015-07-28 14:13:27 -0600

    working properly, need to do some major refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var people = require('../02_people_and_addresses')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  people.getPeople().then(function(people){
+    res.render('index', { title: 'Express', people: people});
+  })
 });
 
 module.exports = router;
